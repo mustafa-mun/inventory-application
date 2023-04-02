7bc3ab8d9771af6cb5fe69ed8330fd76b190657c
Mustafa <mustafamcm9@gmail.com>
2023-04-02 13:48:41 +0300

    item_detail   Implementation of item_detail contoller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,3 @@
-const { collection } = require("../models/item");
 const Item = require("../models/item");
 
 // Display list of all books.
@@ -8,20 +7,30 @@ exports.item_list = (req, res, next) => {
       const items = await Item.find({});
       return items;
     } catch (error) {
+      return next(error);
+    }
+  }
+  getItems().then((results) => {
+    res.render("item_list", {
+      title: "All Items",
+      collection: results,
+    });
+  });
+};
+
+exports.item_detail = (req, res, next) => {
+  async function findItem() {
+    try {
+      const item = await Item.findOne({ _id: req.params.id }).populate("category");
+      return item
+    } catch (error) {
       return next(error)
     }
   }
-  getItems()
-  .then(results => {
-    res.render("item_list", {
-      title: "All Items",
-      collection: results
-    })
-  })
-};
 
-exports.item_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Item detail id ${req.params.id}`);
+  findItem().then((result) => {
+    res.render("item_detail", { item: result });
+  });
 };
 
 exports.item_create_get = (req, res) => {
