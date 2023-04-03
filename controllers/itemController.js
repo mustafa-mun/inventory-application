452434dd937eae0cc897d98f57dc3608f5011264
Mustafa <mustafamcm9@gmail.com>
2023-04-03 21:16:25 +0300

    item update   Implementation of get and post requests for update and authentication

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -132,10 +132,106 @@ exports.item_delete_post = [
   },
 ];
 
-exports.item_update_get = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Item update id ${req.params.id}`);
+exports.item_update_get = (req, res, next) => {
+  // Auth user before update form
+  crudFunction
+    .findDocumentWithID(Item, req.params.id, next)
+    .then((result) => [
+      res.render("password", { title: "Enter Admin Password", item: result }),
+    ]);
 };
 
-exports.item_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Item update post");
-};
+exports.item_update_handle = [
+  // Check if passwor is correct
+  body("password", "Admin password is not correct!").equals(
+    process.env.ADMIN_PASSWORD
+  ),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors, render password page again with errors
+      crudFunction
+        .findDocumentWithID(Item, req.params.id, next)
+        .then((result) => {
+          res.render("password", {
+            title: "Enter Admin Password!",
+            item: result,
+            errors: errors.array(),
+          });
+        });
+    } else {
+      // Password is true, render update form.
+      Promise.all([
+        // Find category with url id (This step is for filling input fields with current item data)
+        crudFunction.findDocumentWithID(Item, req.params.id, next),
+        // Get all categories for select input
+        crudFunction.getAllCollectionDocuments(Category, next),
+      ]).then((results) => {
+        res.render("item_create", {
+          title: "Update Item",
+          item: results[0],
+          categories: results[1],
+        });
+      });
+    }
+  },
+];
+
+exports.item_update_post = [
+  // Validate fields
+  body("name", "Name should be at least three characters!")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Description field can't be empty!")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price should be between 0.1 and 999999").isFloat({
+    min: 0.1,
+    max: 999999,
+  }),
+  body(
+    "number_in_stocks",
+    "Number in stock should be between 1 and 1000"
+  ).isFloat({ min: 1, max: 1000 }),
+  (req, res, next) => {
+    // Extracts errors from request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Get all Category documents
+      crudFunction.getAllCollectionDocuments(Category, next).then((result) => {
+        // Render the form again with sanitized values/errors messages
+        res.render("item_create", {
+          title: "Create Item",
+          categories: result,
+          item: req.body,
+          errors: errors.array(),
+        });
+      });
+      return; // Return after sending the first response
+    }
+    // Form data is valid
+    // Create item object with updated data
+
+    const updatedItem = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stocks: req.body.number_in_stocks,
+      _id: req.params.id,
+    });
+
+    crudFunction.updateDocumentWithID(
+      Item,
+      req.params.id,
+      res,
+      updatedItem,
+      next
+    );
+  },
+];
